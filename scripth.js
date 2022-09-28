5b3b58d5222fc82bce8d7a0f0b7867b8cb73f007
sofia pelaez <106409398+H-sofipelaez@users.noreply.github.com>
2022-09-28 17:00:17 -0500

    Update scripth.js

--- scripth.js
+++ scripth.js
@@ -2,7 +2,7 @@
 let time = new Date ()
 let deltaTime = 0;
 
-if (document.readyState=== "complete"|| document,readyState ==="interactive"){
+if (document.readyState=== "complete"|| document.readyState ==="interactive"){
     setTimeout(init, 1)
 
 }else{
@@ -21,7 +21,7 @@ function loop(){
 }
 //logica del juego
 let sueloY = 22
-let vel = 0
+let velY = 0
 let impulso = 900
 let gravedad = 2500
 
@@ -40,25 +40,69 @@ let contenedor
 let dino
 let textopuntaje
 let suelo
-let gameIver
+let gameOver
 
 function Start(){
     suelo = document.querySelector(".suelo")
     contenedor = document.querySelector(".contenedor")
     textopuntaje = document.querySelector(".puntaje")
     dino = document.querySelector(".dino")
+    //añadimos el evento 
+    document.addEventListener("Keydown", HandleKeyDown)
 
 }
+
 function update (){
-    moverSuelo()
+    moversuelo()
+    //
+    MoverDino()
+
+    velY -=  gravedad * deltaTime;
 
 }
-function moverSuelo() {
-    sueloX +=calcularDesplazamiento()
-    suelo.style.left = -(sueloX % contenedor.clientWidth)+ "px"
+
+function moversuelo(){
+
+    sueloX += calcularDesplazamiento()
+    suelo.style.left = -(sueloX % contenedor.clientWidth) + "px";
 }
+
 function calcularDesplazamiento(){
     return velEscenario * deltaTime * gamevel
 }
+
+function HandleKeyDown(evento){
+    if (evento.KeyCode == 32){
+        Saltar();
+    }
+}
+
+function Saltar(){
+    if (dinoPosY === sueloY){
+        saltando = true
+        velY = impulso
+        dino.classList.remove("dino-corriendo");
+    }
+} 
+
+function MoverDino(){
+    dinoPosY += velY * deltaTime;
+    if (dinoPosY < sueloY){
+        TocarSuelo();
+    }
+    dino.style.bottom = dinoPosY + "px";
+
+}  
+
+function TocarSuelo (){
+    dinoPosY = sueloY;
+    velY = 0;
+
+    if (saltando){
+        dino.classList.add("dino-corriendo")
+    }
+    saltando = false
+}
+
 
 
